50eb610335b23b8f272b1c51ea4637de3259415e
Ashish Ranjan <ashishr@WEB.NET>
2022-03-01 16:03:47 +0530

    code removed

--- src/App.js
+++ src/App.js
@@ -21,11 +21,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

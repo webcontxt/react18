d3554c198ef22c85f3f7b88561eaecc042ce07e0
Ashish Ranjan <ashishr@WEB.NET>
2022-03-01 15:55:12 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
         </a>
